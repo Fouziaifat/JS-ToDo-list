65bc69bff3423e5d82d42ae1556a1967d024d42b
Fouzia ifat <105037033+Fouziaifat@users.noreply.github.com>
2023-03-04 23:56:29 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// 
+let item = document.querySelector("#itam");
+let toDoBox = document.querySelector("#to-do-box");
+
+item.addEventListener (
+    "keyup",
+    function(event) {
+        if(event.key == "Enter"){
+            addToDo(this.value);
+            this.value = ""
+        }
+    }
+);
+let addToDo = (item) => {
+    let listItem =document.createElement("li");
+    listItem.innerHTML =`
+    ${item}
+    <i class="fa fa-times"></i>
+    `;
+    listItem.addEventListener(
+        "click",
+        function(){
+            this.classList.toggle("done")
+        }
+    )
+    listItem.querySelector("i").addEventListener()
+
+    toDoBox.appendChild(listItem);
+}
